ddba64af647663e9e76353a044cf18f3ad44e3be
Ilzumi Zeminori <zemtzov7.yandex.ru>
2026-01-03 23:03:59 +0500

    Rebrand to Luanti Commander

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# minetest-mod-mtfm
+# Luanti Commander
 Formspec-based file manager with text editor and image viewer  
-[![ContentDB](https://content.minetest.net/packages/Zemtzov7/mtfm/shields/downloads/)](https://content.minetest.net/packages/Zemtzov7/mtfm/)
+[![ContentDB](https://content.luanti.org/packages/Zemtzov7/lc/shields/downloads/)](https://content.minetest.net/packages/Zemtzov7/lc/)
 ### Usage
-* Chatcommand `/mtfm [path]` - open file manager, optionally path(absolute) can be specified
+* Chatcommand `/lc [path]` - open file manager, optionally path(absolute) can be specified
 * Click `Modlist` button to get list of installed mods, then you can browse their files
 * `.png` files are opened in image viewer
 * `.ogg` files are played by opening
 * Empty files can not be auto-opened in text editor, use `Edit` button to force it  
-* Read-only mode can be enabled in settings or in minetest.conf: `mtfm.read_only = true`
+* Read-only mode can be enabled in settings or in minetest.conf: `lc.read_only = true`
 * Set `secure.enable_security = false` in the `minetest.conf` to get full RW access to your homedir
 
 ### DISCLAIMER
--- init.lua
+++ init.lua
@@ -1,15 +1,21 @@
-local ro = minetest.settings:get("mtfm.read_only")
+--compat
+local ro_legacy = core.settings:get("mtfm.read_only")
+if ro_legacy then
+	core.settings:set_bool("lc.read_only", true)
+end
 
-local mtfm = {}
-mtfm.dirlist = {}
-mtfm.last_path = {}
-mtfm.last_editor_path = {}
-mtfm.saved = {}
-mtfm.selected = {}
-mtfm.selected_mod = {}
-local modlist = minetest.get_modnames() or {"Failed to load modlist"}
-local worldpath = minetest.get_worldpath()
-local F = minetest.formspec_escape
+local ro = core.settings:get("lc.read_only")
+
+local lc = {}
+lc.dirlist = {}
+lc.last_path = {}
+lc.last_editor_path = {}
+lc.saved = {}
+lc.selected = {}
+lc.selected_mod = {}
+local modlist = core.get_modnames() or {"Failed to load modlist"}
+local worldpath = core.get_worldpath()
+local F = core.formspec_escape
 
 local function btoh(val)
 	if type(val) ~= "number" then
@@ -33,13 +39,13 @@ local function isdir(file)
 end
 
 local function fm_fs(name, path)
-	local _, dirlist = pcall(minetest.get_dir_list, path)
+	local _, dirlist = pcall(core.get_dir_list, path)
 	if type(dirlist) ~= "table" then
-		minetest.chat_send_player(name, "Invalid / illegal path")
+		core.chat_send_player(name, "Invalid / illegal path")
 		return
 	end
 	table.sort(dirlist)
-	mtfm.dirlist[name] = table.copy(dirlist)
+	lc.dirlist[name] = table.copy(dirlist)
 	for i,elem in ipairs(dirlist) do
 		local file = io.open(path.."/"..elem, "r")
 		if file then
@@ -49,8 +55,8 @@ local function fm_fs(name, path)
 			dirlist[i] = dirlist[i]..",<error>"
 		end
 	end
-	mtfm.last_path[name] = path
-	minetest.show_formspec(name, "mtfm:fm", "size[16,12]" ..
+	lc.last_path[name] = path
+	core.show_formspec(name, "lc:fm", "size[16,12]" ..
 		"set_focus[list]" ..
 		"button[0.1,0;1,1;up;^]" ..
 		"field[1.2,0.3;14.3,1;path;;"..F(path).."]" ..
@@ -71,7 +77,7 @@ local function fm_fs(name, path)
 end
 
 local function modlist_fs(name)
-	minetest.show_formspec(name, "mtfm:modlist", "size[16,12]" ..
+	core.show_formspec(name, "lc:modlist", "size[16,12]" ..
 		"button[0.1,0;1,1;up;^]" ..
 		"textlist[0.1,0.9;15.8,10;list;"..table.concat(modlist,",").."]" ..
 		"button[14.5,11;1.5,1;open;Open]")
@@ -82,14 +88,14 @@ local function editor_fs(name, path, new)
 	if not new then
 		local _, file = pcall(io.open, path)
 		if not file then
-			minetest.chat_send_player(name, "Invalid/illegal path")
+			core.chat_send_player(name, "Invalid/illegal path")
 			return
 		end
 		text = file:read(524288) or "Error loading content" --512kb limit
 		file:close()
 	end
-	mtfm.last_editor_path[name] = path
-	minetest.show_formspec(name, "mtfm:editor", "size[16,12]" ..
+	lc.last_editor_path[name] = path
+	core.show_formspec(name, "lc:editor", "size[16,12]" ..
 		"button[0.1,0;1,1;close;X]" ..
 		"field[1.2,0.3;13.5,1;path;;"..F(path).."]" ..
 		"field_close_on_enter[path;false]" ..
@@ -100,34 +106,34 @@ local function editor_fs(name, path, new)
 end
 
 local function image_viewer_fs(name, path, image)
-	minetest.show_formspec(name, "mtfm:image_viewer", "size[16,12]" ..
+	core.show_formspec(name, "lc:image_viewer", "size[16,12]" ..
 		"button[0.1,0;1,1;close;X]" ..
 		"field[1.2,0.3;14.9,1;path;;"..path.."]" ..
 		"field_close_on_enter[path;false]" ..
 		"image[3,1;12,12;"..image.."]")
 end
 
-minetest.register_chatcommand("mtfm",{
+core.register_chatcommand("lc",{
   description = "Open file manager",
   privs = {server=true},
   params = "[path]",
   func = function(name, param)
-	if not minetest.get_player_by_name(name) then
+	if not core.get_player_by_name(name) then
 		return false, "No player"
 	end
-	fm_fs(name, param and param ~= "" and param or mtfm.last_path[name] or worldpath)
+	fm_fs(name, param and param ~= "" and param or lc.last_path[name] or worldpath)
 	return true, "File manager opened"
 end})
 
-minetest.register_on_player_receive_fields(function(player, formname, fields)
-	if not formname:match("^mtfm:") then return end
+core.register_on_player_receive_fields(function(player, formname, fields)
+	if not formname:match("^lc:") then return end
 	local name = player:get_player_name()
 	if not name then return end
-	if formname == "mtfm:fm" then
-		local dirlist = mtfm.dirlist[name]
+	if formname == "lc:fm" then
+		local dirlist = lc.dirlist[name]
 		if fields.list then
-			local evnt = minetest.explode_table_event(fields.list)
-			mtfm.selected[name] = evnt.row
+			local evnt = core.explode_table_event(fields.list)
+			lc.selected[name] = evnt.row
 		end
 		if (fields.go or fields.key_enter_field) and fields.path and fields.path ~= "" then
 			fm_fs(name, fields.path)
@@ -139,37 +145,37 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 			fm_fs(name, worldpath)
 		end
 		if fields.edit then
-			local elem = dirlist[mtfm.selected[name]]
+			local elem = dirlist[lc.selected[name]]
 			if not elem or elem == "" then
-				minetest.chat_send_player(name, "Please select file/dir")
+				core.chat_send_player(name, "Please select file/dir")
 				return
 			end
-			editor_fs(name,mtfm.last_path[name].."/"..elem)
+			editor_fs(name,lc.last_path[name].."/"..elem)
 		end
 		if fields.open or (fields.list and fields.list:match("DCL")) then
-			local elem = dirlist[mtfm.selected[name]]
+			local elem = dirlist[lc.selected[name]]
 			if not elem or elem == "" then
-				minetest.chat_send_player(name, "Please select file/dir")
+				core.chat_send_player(name, "Please select file/dir")
 				return
 			end
 			if elem:match("%S+%.png$") then
-				image_viewer_fs(name, mtfm.last_path[name].."/"..elem, elem)
+				image_viewer_fs(name, lc.last_path[name].."/"..elem, elem)
 				return
 			end
 			if elem:match("%S+%.ogg$") then
-				minetest.sound_play(elem:gsub(".ogg",""), {to_player=name})
+				core.sound_play(elem:gsub(".ogg",""), {to_player=name})
 				return
 			end
-			local _, file = pcall(io.open, mtfm.last_path[name].."/"..elem)
+			local _, file = pcall(io.open, lc.last_path[name].."/"..elem)
 			if file then
 				if isdir(file) then
-					fm_fs(name, mtfm.last_path[name].."/"..elem)
+					fm_fs(name, lc.last_path[name].."/"..elem)
 				else
-					editor_fs(name, mtfm.last_path[name].."/"..elem)
+					editor_fs(name, lc.last_path[name].."/"..elem)
 				end
 				file:close()
 			else
-				minetest.chat_send_player(name, "Error opening")
+				core.chat_send_player(name, "Error opening")
 			end
 		end
 		if fields.up and fields.path and fields.path ~= "" then
@@ -178,69 +184,69 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 			fm_fs(name, newpath)
 		end
 		if fields.newdir and not ro then
-			minetest.show_formspec(name, "mtfm:newdir_dialog", "size[8,2]"..
+			core.show_formspec(name, "lc:newdir_dialog", "size[8,2]"..
 				"field[0.3,0.1;8,1;dirname;;NewDir]"..
 				"field_close_on_enter[path;false]"..
 				"button[3,1;2,1;accept;Accept]")
 		end
 		if fields.newfile and not ro then
-			editor_fs(name,mtfm.last_path[name].."/new.txt", true)
+			editor_fs(name,lc.last_path[name].."/new.txt", true)
 		end
 		if fields.del and not ro then
-			local elem = dirlist[mtfm.selected[name]]
+			local elem = dirlist[lc.selected[name]]
 			if not elem or elem == "" then
-				minetest.chat_send_player(name, "Please select file/dir")
+				core.chat_send_player(name, "Please select file/dir")
 				return
 			end
-			local _, ok = pcall(minetest.rmdir, mtfm.last_path[name].."/"..elem, true)
-			minetest.chat_send_player(name, ok and "Deleted successfully" or "Error deleting")
-			fm_fs(name, mtfm.last_path[name])
+			local _, ok = pcall(core.rmdir, lc.last_path[name].."/"..elem, true)
+			core.chat_send_player(name, ok and "Deleted successfully" or "Error deleting")
+			fm_fs(name, lc.last_path[name])
 		end
 		if fields.copy and not ro then
-			local elem = dirlist[mtfm.selected[name]]
+			local elem = dirlist[lc.selected[name]]
 			if not elem or elem == "" then
-				minetest.chat_send_player(name, "Please select file/dir")
+				core.chat_send_player(name, "Please select file/dir")
 				return
 			end
-			local _, file = pcall(io.open, mtfm.last_path[name].."/"..elem, "r")
+			local _, file = pcall(io.open, lc.last_path[name].."/"..elem, "r")
 			if file then
-				local _, ok = isdir(file) and pcall(minetest.cpdir, mtfm.last_path[name].."/"..elem, mtfm.last_path[name].."/"..elem.."-copy")
-					or pcall(minetest.safe_file_write, mtfm.last_path[name].."/"..elem.."-copy", file:read("*a"))
-				minetest.chat_send_player(name, ok and "Copied successfully" or "Error copying")
+				local _, ok = isdir(file) and pcall(core.cpdir, lc.last_path[name].."/"..elem, lc.last_path[name].."/"..elem.."-copy")
+					or pcall(core.safe_file_write, lc.last_path[name].."/"..elem.."-copy", file:read("*a"))
+				core.chat_send_player(name, ok and "Copied successfully" or "Error copying")
 				file:close()
 			else
-				minetest.chat_send_player(name, "Error copying")
+				core.chat_send_player(name, "Error copying")
 			end
-			fm_fs(name, mtfm.last_path[name])
+			fm_fs(name, lc.last_path[name])
 		end
 	end
-	if formname == "mtfm:modlist" then
+	if formname == "lc:modlist" then
 		if fields.list then
-			local evnt = minetest.explode_textlist_event(fields.list)
-			mtfm.selected_mod[name] = evnt.index
+			local evnt = core.explode_textlist_event(fields.list)
+			lc.selected_mod[name] = evnt.index
 		end
 		if fields.up then
 			fm_fs(name,worldpath)
 		end
 		if fields.open or (fields.list and fields.list:match("DCL")) then
-			local modname = modlist[mtfm.selected_mod[name]]
+			local modname = modlist[lc.selected_mod[name]]
 			if not modname or modname == "" then
-				minetest.chat_send_player(name, "Please select a mod")
+				core.chat_send_player(name, "Please select a mod")
 				return
 			end
-			fm_fs(name, minetest.get_modpath(modlist[mtfm.selected_mod[name]]))
+			fm_fs(name, core.get_modpath(modlist[lc.selected_mod[name]]))
 		end
 	end
-	if formname == "mtfm:newdir_dialog" then
+	if formname == "lc:newdir_dialog" then
 		if (fields.accept or fields.key_enter_field) and fields.dirname and fields.dirname ~= "" and not ro then
-			local _, ok = pcall(minetest.mkdir, mtfm.last_path[name].."/"..fields.dirname)
-			minetest.chat_send_player(name, ok and "Successfully created new directory" or "Error creating new directory")
-			fm_fs(name, mtfm.last_path[name])
+			local _, ok = pcall(core.mkdir, lc.last_path[name].."/"..fields.dirname)
+			core.chat_send_player(name, ok and "Successfully created new directory" or "Error creating new directory")
+			fm_fs(name, lc.last_path[name])
 		end
 	end
-	if formname == "mtfm:editor" then
+	if formname == "lc:editor" then
 		if fields.close then
-			fm_fs(name, mtfm.last_path[name])
+			fm_fs(name, lc.last_path[name])
 		end
 		if (fields.open or fields.key_enter_field) and fields.path and fields.path ~= "" then
 			editor_fs(name, fields.path)
@@ -248,14 +254,14 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 		if fields.save and fields.path and fields.path ~= "" and not ro then
 			local content = fields.content
 			if content then
-				local _, ok = pcall(minetest.safe_file_write, fields.path, content)
-				minetest.chat_send_player(name, ok and "Saved successfully" or "Erorr saving file")
+				local _, ok = pcall(core.safe_file_write, fields.path, content)
+				core.chat_send_player(name, ok and "Saved successfully" or "Erorr saving file")
 			end
 		end
 	end
-	if formname == "mtfm:image_viewer" then
+	if formname == "lc:image_viewer" then
 		if fields.close then
-			fm_fs(name, mtfm.last_path[name])
+			fm_fs(name, lc.last_path[name])
 		end
 	end
 end)
